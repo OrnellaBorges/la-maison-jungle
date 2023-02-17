f590afc3cfacc23ea52f45e49cb239cca9b1c5d1
Ornella B <borges.ornella@gmail.com>
2023-02-17 22:02:24 +0100

    P2C1- create components export import and clean

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Banner from './banner';
+import Cart from './cart.js';
+
+function App() {
+  return (
+    <div className="body">
+        <Banner />
+        <Cart/>
+    </div>
+
+  )
+
+}
+
+export default App;
