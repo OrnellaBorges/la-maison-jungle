462b70a3cef139e6491941d77d49d4c2d20dbc70
Ornella B <borges.ornella@gmail.com>
2023-02-18 12:48:08 +0100

    commit

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,14 @@
+import '../styles/banner.css'
+import logo from '../assets/logo.png'
+
 function Banner(){
-    return <h1>La maison jungle</h1>
+    return (
+    
+    <div className="lmj-banner">
+        <img src={logo} alt = 'logo la maison jungle'></img>
+        <h1>La maison jungle</h1>
+    </div>
+    )
 }
 
 export default Banner
